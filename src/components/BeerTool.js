2626b0946b18cc92142e6469fa66b0d715b3e176
Mattijn Lahuis <mattijnlahuis@gmail.com>
2017-06-07 22:34:27 +0200

    added input for desired amount

--- src/components/BeerTool.js
+++ src/components/BeerTool.js
@@ -8,31 +8,59 @@ class BeerTool extends PureComponent {
 
     this.state = {
       recipe: {
-        textAreaValue: ''
+        yieldInLiters: 25
       }
     }
+
+    this.updateRecipeState = this.updateRecipeState.bind(this)
+    this.handleRecipeInput = this.handleRecipeInput.bind(this)
+    this.handleLitersInput = this.handleLitersInput.bind(this)
+  }
+
+  updateRecipeState(newValues = {}) {
+    this.setState({
+      recipe: Object.assign(
+        {},
+        this.state.recipe,
+        newValues)
+      })
   }
 
   handleRecipeInput(event) {
     const recipeInput = event.target.value
+    const newValues = {}
 
-    const result = recipeInput.match(/Ingredients:([\s\S]*?)Additional Instructions/)
+    const yieldMatch = recipeInput.match(/Yield: ([\s\S]*?)\n/)
 
-    if(result) {
-      const ingredients = result[1].replace(/^\s+|\s+$/g, '').replace(/\n\s+/g, '\n')
+    if(yieldMatch) {
+      //we expect the string to begin with the value, e.g. 5.75 US gallons
+      const yieldValue = yieldMatch[1].split(" ")[0]
 
-      console.log(ingredients)
-
-      this.setState({
-        recipe: Object.assign(
-          {},
-          this.state.recipe,
-          {
-            ingredients: ingredients,
-            textAreaValue: event.target.value
-          })
-      })
+      if(!isNaN(yieldValue)) {
+        newValues.yieldValue = yieldValue
+        newValues.yieldString = yieldMatch[1]
+      }
     }
+
+    const ingredientsMatch = recipeInput.match(/Ingredients:([\s\S]*?)Additional Instructions/)
+
+    if(ingredientsMatch) {
+      const ingredients = ingredientsMatch[1]
+        //remove preceding and trailing white spaces, new lines, carriage returns
+        .replace(/^\s+|\s+$/g, '')
+        //remove white spaces before every line of ingredients
+        .replace(/\n\s+/g, '\n')
+
+      newValues.ingredients = ingredients
+      newValues.textAreaValue = event.target.value
+    }
+    this.updateRecipeState(newValues)
+  }
+
+  handleLitersInput(event) {
+    this.updateRecipeState({
+      yieldInLiters: event.target.value
+    })
   }
 
   render() {
@@ -45,7 +73,7 @@ class BeerTool extends PureComponent {
           placeholder='Paste recipe here'
           ref='recipeTextArea'
           value={this.state.recipe.textAreaValue}
-          onChange={this.handleRecipeInput.bind(this)}
+          onChange={this.handleRecipeInput}
         >
         </textarea>
 
@@ -55,7 +83,22 @@ class BeerTool extends PureComponent {
               <div className="ingredientCard">
                 <IngredientCard ingredients={this.state.recipe.ingredients} />
                 <div>
-                  <input type="text" />
+                  { this.state.recipe.yieldValue ? (
+                      <div>
+                        <h3>Yield</h3>
+                        <div>Original yield: { this.state.recipe.yieldString }</div>
+                        <div>
+                          Desired yield in not stupid system:
+                          <input
+                            type="text"
+                            ref='yieldInLiters'
+                            value={this.state.recipe.yieldInLiters}
+                            onChange={this.handleLitersInput}
+                          />
+                        </div>
+                      </div>
+                    ) : null
+                  }
                 </div>
                 <IngredientCard ingredients={this.state.recipe.ingredients} />
               </div>
